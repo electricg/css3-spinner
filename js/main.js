b845af961a6f479980d7fb8393777bcad9969553
electricg <electric.g@gmail.com>
2013-01-19 13:59:34 +0000

    First working version

---
+++ js/main.js
@@ -0,0 +1,85 @@
+function loader(opts) {
+	// var size = 10,			// px
+	// 	points = 5,
+	// 	ray = 100,			// px
+	// 	color1 = '#000',
+	// 	color2 = '#FFF',
+	// 	speedN = 2;		// seconds
+
+	var size = new Number(opts.size),			// px
+		points = new Number(opts.points),
+		loaderSize = new Number(opts.loaderSize),			// px
+		color1 = opts.color1,
+		color2 = opts.color2,
+		speedN = new Number(opts.speed);		// seconds
+
+	var string = '',
+		umLenght = 'px',
+		umTime = 's',
+		animationName = 'shadow',
+		ray = (loaderSize - size) / 2,
+		//angle = 360 / points,	// deg
+		angle = Math.PI * 2 / points, // radians
+		loaderHeight = loaderWidth = loaderSize,
+		speed = speedN + umTime,
+		frame = 100 / points;
+
+	function shadow(sel) {
+		var result = [],
+			color,
+			x = y = 0;
+		for (var i = 0; i <= points; i++) {
+			x = (ray * Math.cos(angle * i)).toFixed(5);
+			y = (ray * Math.sin(angle * i)).toFixed(5);
+			color = color1;
+			if (sel === i) {
+				color = color2;
+			}
+			result.push( [x + umLenght, y + umLenght, color].join(' ') );
+		}
+		return result.join(',');
+	}
+
+	function keyframes() {
+		var time,
+			result = '';
+		for (var i = 0; i <= points; i++) {
+			time = frame * i;
+			result += "\
+				" + time + "% {\
+					box-shadow: " + shadow(i - 1) + ";\
+				}\
+			";
+		}
+		return result;
+	}
+
+
+	string = "\
+	.loader {\
+		height: " + loaderHeight + umLenght + ";\
+		width: " + loaderWidth + umLenght + ";\
+		position: relative;\
+	}\
+	.loader:after {\
+		background: transparent;\
+		border-radius: 100%;\
+		content: '';\
+		display: block;\
+		height: " + size + umLenght + ";\
+		width: " + size + umLenght + ";\
+		position: absolute;\
+		top: " + ray + umLenght + ";\
+		left: " + ray + umLenght +";\
+	\
+		-webkit-animation: " + animationName + " " + speed + " infinite linear;\
+		        animation: " + animationName + " " + speed + " infinite linear;\
+		box-shadow: " + shadow(0) + ";\
+	}\
+	@-webkit-keyframes shadow {\
+		" + keyframes() + "\
+	}\
+	";
+
+	return string;
+}
